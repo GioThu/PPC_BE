218aa7f7d9bc1735c070ea7564a9bda75ae7f0bb
GioThu <htxtrungkien2002@gmail.com>
2025-07-26 00:48:07 +0700

    fix description chapter

--- PCMS_GSU25SE26_BE/PPC.Service/ModelResponse/CourseResponse/ChapterDetailDto.cs
+++ PCMS_GSU25SE26_BE/PPC.Service/ModelResponse/CourseResponse/ChapterDetailDto.cs
@@ -11,6 +11,7 @@ namespace PPC.Service.ModelResponse.CourseResponse
         public string Id { get; set; }
         public string Name { get; set; }
         public int? ChapNum { get; set; }
+        public string Description { get; set; }
         public string ChapterType { get; set; }
 
         public LectureDto Lecture { get; set; }
