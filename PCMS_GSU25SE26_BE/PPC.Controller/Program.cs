a45f7f9ab62761b4a51a0f485f1322a16703e96e
GioThu <htxtrungkien2002@gmail.com>
2025-08-09 19:15:27 +0700

    fix cors

--- PCMS_GSU25SE26_BE/PPC.Controller/Program.cs
+++ PCMS_GSU25SE26_BE/PPC.Controller/Program.cs
@@ -147,7 +147,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "https://www.ppccapstone.somee.com", "http://localhost:7013", "https://ccp-two.vercel.app")
+        policy.WithOrigins("http://localhost:3000", "https://www.ppccapstone.somee.com", "http://localhost:7013", "https://ccp-two.vercel.app", "https://ccp-mu.vercel.app")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
